1dea43471d2ac240166fe9fbad0d585d218d2229
NKrein <nikokrein@gmail.com>
2022-05-04 10:16:56 -0300

    ENV implemented for firebase security

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -5,12 +5,12 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDgYpHUr3eq71y_3HKD0yazCQ4BQqVoUiQ",
-  authDomain: "dharmatienda-8009b.firebaseapp.com",
-  projectId: "dharmatienda-8009b",
-  storageBucket: "dharmatienda-8009b.appspot.com",
-  messagingSenderId: "333571628022",
-  appId: "1:333571628022:web:acf4d6729c92fd4094c5d7"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
